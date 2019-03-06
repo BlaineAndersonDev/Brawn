26eebaa726630fc12af050354132b9e9d1af1681
Blaine Anderson <BlaineAndersonDev@gmail.com>
2019-03-06 14:19:54 -0700

    Added additional folder for orginization

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.js';
 import List from './pages/List.js';
-import Title from './pages/Title.js';
+import Title from './pages/titles/Title.js';
 
 class App extends Component {
   render() {
